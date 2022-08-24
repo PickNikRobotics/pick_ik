716f92143b93368840d7d4180d5078c2eb4cf21a
Tyler Weaver <tyler@picknik.ai>
2022-08-24 10:04:10 -0600

    Remove GoalContex
    Signed-off-by: Tyler Weaver <tyler@picknik.ai>

--- include/gd_ik/goal.hpp
+++ include/gd_ik/goal.hpp
@@ -12,24 +12,6 @@
 
 namespace gd_ik {
 
-struct GoalContext {
-  std::vector<double> active_variable_positions;
-  std::vector<Frame> tip_link_frames;
-  std::vector<ssize_t> goal_variable_indices;
-  std::vector<size_t> goal_link_indices;
-  bool goal_secondary;
-  std::vector<std::string> goal_link_names;
-  std::vector<std::string> goal_variable_names;
-  double goal_weight;
-  moveit::core::JointModelGroup const* joint_model_group;
-  std::vector<size_t> problem_active_variables;
-  std::vector<size_t> problem_tip_link_indices;
-  std::vector<double> initial_guess;
-  std::vector<double> velocity_weights;
-  Robot robot;
-  std::vector<double> temp_vector;
-};
-
 // Goal Function type
 using CostFn =
     std::function<double(std::vector<Frame> const& tip_frames,
