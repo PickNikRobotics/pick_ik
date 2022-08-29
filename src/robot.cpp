8312ae66d2c14a773b5c9a8fd4642c62b1dd0f18
Tyler Weaver <maybe@tylerjw.dev>
2022-08-29 12:39:01 -0600

    Add headers for convering geometry msg to Eigen type

--- src/robot.cpp
+++ src/robot.cpp
@@ -2,6 +2,10 @@
 #include <gd_ik/math.hpp>
 #include <gd_ik/robot.hpp>
 
+#include <tf2_eigen/tf2_eigen.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
+#include <Eigen/Geometry>
 #include <cfloat>
 #include <cmath>
 #include <fmt/core.h>
