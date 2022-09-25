44cf7b3620db0938dceef2fc742772c1312a940f
Tyler Weaver <maybe@tylerjw.dev>
2022-09-25 19:15:13 +0000

    Pose cost function tests

--- tests/goal_tests.cpp
+++ tests/goal_tests.cpp
@@ -4,6 +4,151 @@
 #include <catch2/catch_all.hpp>
 
 TEST_CASE("pick_ik::make_pose_cost_fn") {
+    SECTION("Goal is frame") {
+        auto const goal = pick_ik::Frame{
+            tf2::Vector3(0, 0, 0),
+            tf2::Quaternion(0, 0, 0, 1),
+        };
+
+        auto const cost_fn = pick_ik::make_pose_cost_fn(goal, 0, 0);
+        CHECK(cost_fn({goal}) == Catch::Approx(0.));
+    }
+
+    SECTION("Goal is frame, with rotation scale") {
+        auto const goal = pick_ik::Frame{
+            tf2::Vector3(0, 0, 0),
+            tf2::Quaternion(0, 0, 0, 1),
+        };
+
+        auto const cost_fn = pick_ik::make_pose_cost_fn(goal, 0, 0.5);
+        CHECK(cost_fn({goal}) == Catch::Approx(0.));
+    }
+
+    SECTION("Goal is second index") {
+        auto const goal = pick_ik::Frame{
+            tf2::Vector3(0, 2, 0),
+            tf2::Quaternion(0, 0, 0, 1),
+        };
+
+        auto const cost_fn = pick_ik::make_pose_cost_fn(goal, 1, 0);
+        CHECK(cost_fn({pick_ik::Frame{}, goal}) == Catch::Approx(0.));
+    }
+
+    SECTION("Translation along one axis square of distance") {
+        auto const goal = pick_ik::Frame{
+            tf2::Vector3(0, 0, 0),
+            tf2::Quaternion(0, 0, 0, 1),
+        };
+        auto const frame = pick_ik::Frame{
+            tf2::Vector3(2, 0, 0),
+            tf2::Quaternion(0, 0, 0, 1),
+        };
+
+        auto const cost_fn = pick_ik::make_pose_cost_fn(goal, 0, 0.5);
+        CHECK(cost_fn({frame}) == Catch::Approx(std::pow(2., 2)));
+    }
+
+    SECTION("Zero rotation scale with rotation") {
+        auto const goal = pick_ik::Frame{
+            tf2::Vector3(0, 0, 0),
+            tf2::Quaternion(0, 0, 0, 1),
+        };
+        auto const frame = pick_ik::Frame{
+            tf2::Vector3(0, 0, 0),
+            tf2::Quaternion(1, 0, 0, 1),
+        };
+
+        auto const cost_fn = pick_ik::make_pose_cost_fn(goal, 0, 0);
+        CHECK(cost_fn({frame}) == Catch::Approx(0.));
+    }
+
+    SECTION("Negative rotation same as zero rotation scale") {
+        auto const goal = pick_ik::Frame{
+            tf2::Vector3(0, 0, 0),
+            tf2::Quaternion(0, 0, 0, 1),
+        };
+        auto const frame = pick_ik::Frame{
+            tf2::Vector3(1, 0, 0),
+            tf2::Quaternion(1, 0, 0, 1),
+        };
+
+        auto const cost_fn_zero = pick_ik::make_pose_cost_fn(goal, 0, 0.);
+        auto const cost_fn_neg = pick_ik::make_pose_cost_fn(goal, 0, -0.5);
+        CHECK(cost_fn_zero({frame}) == cost_fn_neg({frame}));
+    }
+
+    SECTION("Translation in two axis, square of distance") {
+        auto const goal = pick_ik::Frame{
+            tf2::Vector3(0, 0, 0),
+            tf2::Quaternion(0, 0, 0, 1),
+        };
+        auto const frame = pick_ik::Frame{
+            tf2::Vector3(1, 1, 0),
+            tf2::Quaternion(0, 0, 0, 1),
+        };
+
+        // We know that the distance between (0,0,0) and (1,1,0) is sqrt(2)
+        auto const translation_distance = std::sqrt(2.);
+
+        auto const const_fn = pick_ik::make_pose_cost_fn(goal, 0, 0.5);
+        CHECK(const_fn({frame}) == Catch::Approx(std::pow(translation_distance, 2)));
+    }
+
+    SECTION("Translation in three axis, square of distance") {
+        auto const goal = pick_ik::Frame{
+            tf2::Vector3(0, 0, 0),
+            tf2::Quaternion(0, 0, 0, 1),
+        };
+        auto const frame = pick_ik::Frame{
+            tf2::Vector3(1, 1, 1),
+            tf2::Quaternion(0, 0, 0, 1),
+        };
+
+        // We know that the distance between (0,0,0) and (1,1,1) is sqrt(3)
+        auto const translation_distance = std::sqrt(3.);
+
+        auto const const_fn = pick_ik::make_pose_cost_fn(goal, 0, 0.5);
+        CHECK(const_fn({frame}) == Catch::Approx(std::pow(translation_distance, 2)));
+    }
+
+    SECTION("Rotation in one axis") {
+        auto const goal = pick_ik::Frame{
+            tf2::Vector3(0, 0, 0),
+            tf2::Quaternion(0, 0, 0, 1),
+        };
+        auto const frame = pick_ik::Frame{
+            tf2::Vector3(0, 0, 0),
+            tf2::Quaternion(2, 0, 0, 1),
+        };
+
+        // We know that the distance between (0, 0, 0, 1) and (2, 0, 0, 1) is 2
+        auto const rotational_distance = 2.;
+
+        auto const const_fn = pick_ik::make_pose_cost_fn(goal, 0, 1.0);
+        CHECK(const_fn({frame}) == Catch::Approx(std::pow(rotational_distance, 2)));
+    }
+
+    SECTION("Rotation in one axis, scaled 0.5") {
+        auto const goal = pick_ik::Frame{
+            tf2::Vector3(0, 0, 0),
+            tf2::Quaternion(0, 0, 0, 1),
+        };
+        auto const frame = pick_ik::Frame{
+            tf2::Vector3(0, 0, 0),
+            tf2::Quaternion(2, 0, 0, 1),
+        };
+
+        // We know that the distance between (0, 0, 0, 1) and (2, 0, 0, 1) is 2
+        auto const rotational_distance = 2.;
+        auto const rotation_scale = 0.5;
+
+        auto const const_fn = pick_ik::make_pose_cost_fn(goal, 0, rotation_scale);
+
+        // The rotation scale is squared before being multiplied by the square of the distance
+        CHECK(const_fn({frame}) ==
+              Catch::Approx(std::pow(rotational_distance, 2) * std::pow(rotation_scale, 2)));
+    }
+
     SECTION("Test 0 - sample data taken from instrumenting bio_ik") {
         auto const goal = pick_ik::Frame{
             tf2::Vector3(0.3548182547092438, -0.04776066541671753, 0.5902695655822754),
