102658485510d204f453b0c8e0e5d5ef7f9d087b
Tyler Weaver <maybe@tylerjw.dev>
2022-09-25 14:33:50 -0600

    make_frame_tests tests

--- tests/goal_tests.cpp
+++ tests/goal_tests.cpp
@@ -5,149 +5,139 @@
 
 namespace pick_ik {
 
-TEST_CASE("make_pose_cost_fn") {
-    SECTION("Goal is frame") {
-        auto const goal = Frame{
-            tf2::Vector3(0, 0, 0),
-            tf2::Quaternion(0, 0, 0, 1),
+TEST_CASE("make_frame_tests") {
+    auto const epsilon = 0.00001;
+    auto const zero_frame = Frame{
+        tf2::Vector3(0, 0, 0),
+        tf2::Quaternion(0, 0, 0, 1),
+    };
+
+    SECTION("One goal, one test") {
+        auto const test_fns = make_frame_tests({zero_frame}, epsilon);
+        CHECK(test_fns.size() == 1);
+    }
+
+    SECTION("Zero threshold") {
+        auto const test_fns = make_frame_tests({zero_frame}, 0.);
+        CHECK(test_fns.at(0)({zero_frame}) == false);
+    }
+
+    SECTION("Goal is almost frame, but not quite") {
+        auto const frame = Frame{
+            tf2::Vector3(epsilon, epsilon, epsilon),
+            tf2::Quaternion(0, 0, epsilon, 1 - epsilon),
         };
 
-        auto const cost_fn = make_pose_cost_fn(goal, 0, 0);
-        CHECK(cost_fn({goal}) == Catch::Approx(0.));
+        auto const test_fns = make_frame_tests({zero_frame}, epsilon);
+        CHECK(test_fns.at(0)({frame}) == false);
+    }
+
+    SECTION("Goal is almost frame, within threshold") {
+        auto const frame = Frame{
+            tf2::Vector3(0, 0.000009, 0),
+            tf2::Quaternion(0, 0, 0.000001, 0.99999),
+        };
+
+        auto const test_fns = make_frame_tests({zero_frame}, epsilon);
+        CHECK(test_fns.at(0)({frame}) == true);
+    }
+
+    SECTION("Goal is frame") {
+        auto const test_fns = make_frame_tests({zero_frame}, epsilon);
+        CHECK(test_fns.at(0)({zero_frame}) == true);
+    }
+}
+
+TEST_CASE("make_pose_cost_fn") {
+    auto const zero_frame = Frame{
+        tf2::Vector3(0, 0, 0),
+        tf2::Quaternion(0, 0, 0, 1),
+    };
+    auto const translate_y2_frame = Frame{
+        tf2::Vector3(0, 2, 0),
+        tf2::Quaternion(0, 0, 0, 1),
+    };
+    auto const translate_xy1_frame = Frame{
+        tf2::Vector3(1, 1, 0),
+        tf2::Quaternion(0, 0, 0, 1),
+    };
+    auto const translate_xyz1_frame = Frame{
+        tf2::Vector3(1, 1, 1),
+        tf2::Quaternion(0, 0, 0, 1),
+    };
+    auto const rotate_x1_frame = Frame{
+        tf2::Vector3(0, 0, 0),
+        tf2::Quaternion(1, 0, 0, 1),
+    };
+    auto const rotate_y2_frame = Frame{
+        tf2::Vector3(0, 0, 0),
+        tf2::Quaternion(0, 2, 0, 1),
+    };
+
+    SECTION("Goal is frame") {
+        auto const cost_fn = make_pose_cost_fn(zero_frame, 0, 0);
+        CHECK(cost_fn({zero_frame}) == Catch::Approx(0.));
     }
 
     SECTION("Goal is frame, with rotation scale") {
-        auto const goal = Frame{
-            tf2::Vector3(0, 0, 0),
-            tf2::Quaternion(0, 0, 0, 1),
-        };
-
-        auto const cost_fn = make_pose_cost_fn(goal, 0, 0.5);
-        CHECK(cost_fn({goal}) == Catch::Approx(0.));
+        auto const cost_fn = make_pose_cost_fn(zero_frame, 0, 0.5);
+        CHECK(cost_fn({zero_frame}) == Catch::Approx(0.));
     }
 
     SECTION("Goal is second index") {
-        auto const goal = Frame{
-            tf2::Vector3(0, 2, 0),
-            tf2::Quaternion(0, 0, 0, 1),
-        };
-
-        auto const cost_fn = make_pose_cost_fn(goal, 1, 0);
-        CHECK(cost_fn({Frame{}, goal}) == Catch::Approx(0.));
+        auto const cost_fn = make_pose_cost_fn(translate_y2_frame, 1, 0);
+        CHECK(cost_fn({zero_frame, translate_y2_frame}) == Catch::Approx(0.));
     }
 
     SECTION("Translation along one axis square of distance") {
-        auto const goal = Frame{
-            tf2::Vector3(0, 0, 0),
-            tf2::Quaternion(0, 0, 0, 1),
-        };
-        auto const frame = Frame{
-            tf2::Vector3(2, 0, 0),
-            tf2::Quaternion(0, 0, 0, 1),
-        };
-
-        auto const cost_fn = make_pose_cost_fn(goal, 0, 0.5);
-        CHECK(cost_fn({frame}) == Catch::Approx(std::pow(2., 2)));
+        auto const cost_fn = make_pose_cost_fn(zero_frame, 0, 0.5);
+        CHECK(cost_fn({translate_y2_frame}) == Catch::Approx(std::pow(2., 2)));
     }
 
     SECTION("Zero rotation scale with rotation") {
-        auto const goal = Frame{
-            tf2::Vector3(0, 0, 0),
-            tf2::Quaternion(0, 0, 0, 1),
-        };
-        auto const frame = Frame{
-            tf2::Vector3(0, 0, 0),
-            tf2::Quaternion(1, 0, 0, 1),
-        };
-
-        auto const cost_fn = make_pose_cost_fn(goal, 0, 0);
-        CHECK(cost_fn({frame}) == Catch::Approx(0.));
+        auto const cost_fn = make_pose_cost_fn(zero_frame, 0, 0);
+        CHECK(cost_fn({rotate_x1_frame}) == Catch::Approx(0.));
     }
 
     SECTION("Negative rotation same as zero rotation scale") {
-        auto const goal = Frame{
-            tf2::Vector3(0, 0, 0),
-            tf2::Quaternion(0, 0, 0, 1),
-        };
-        auto const frame = Frame{
-            tf2::Vector3(1, 0, 0),
-            tf2::Quaternion(1, 0, 0, 1),
-        };
-
-        auto const cost_fn_zero = make_pose_cost_fn(goal, 0, 0.);
-        auto const cost_fn_neg = make_pose_cost_fn(goal, 0, -0.5);
-        CHECK(cost_fn_zero({frame}) == cost_fn_neg({frame}));
+        auto const cost_fn_zero = make_pose_cost_fn(zero_frame, 0, 0.);
+        auto const cost_fn_neg = make_pose_cost_fn(zero_frame, 0, -0.5);
+        CHECK(cost_fn_zero({rotate_x1_frame}) == cost_fn_neg({rotate_x1_frame}));
     }
 
     SECTION("Translation in two axis, square of distance") {
-        auto const goal = Frame{
-            tf2::Vector3(0, 0, 0),
-            tf2::Quaternion(0, 0, 0, 1),
-        };
-        auto const frame = Frame{
-            tf2::Vector3(1, 1, 0),
-            tf2::Quaternion(0, 0, 0, 1),
-        };
-
         // We know that the distance between (0,0,0) and (1,1,0) is sqrt(2)
         auto const translation_distance = std::sqrt(2.);
 
-        auto const const_fn = make_pose_cost_fn(goal, 0, 0.5);
-        CHECK(const_fn({frame}) == Catch::Approx(std::pow(translation_distance, 2)));
+        auto const const_fn = make_pose_cost_fn(zero_frame, 0, 0.5);
+        CHECK(const_fn({translate_xy1_frame}) == Catch::Approx(std::pow(translation_distance, 2)));
     }
 
     SECTION("Translation in three axis, square of distance") {
-        auto const goal = Frame{
-            tf2::Vector3(0, 0, 0),
-            tf2::Quaternion(0, 0, 0, 1),
-        };
-        auto const frame = Frame{
-            tf2::Vector3(1, 1, 1),
-            tf2::Quaternion(0, 0, 0, 1),
-        };
-
         // We know that the distance between (0,0,0) and (1,1,1) is sqrt(3)
         auto const translation_distance = std::sqrt(3.);
 
-        auto const const_fn = make_pose_cost_fn(goal, 0, 0.5);
-        CHECK(const_fn({frame}) == Catch::Approx(std::pow(translation_distance, 2)));
+        auto const const_fn = make_pose_cost_fn(zero_frame, 0, 0.5);
+        CHECK(const_fn({translate_xyz1_frame}) == Catch::Approx(std::pow(translation_distance, 2)));
     }
 
     SECTION("Rotation in one axis") {
-        auto const goal = Frame{
-            tf2::Vector3(0, 0, 0),
-            tf2::Quaternion(0, 0, 0, 1),
-        };
-        auto const frame = Frame{
-            tf2::Vector3(0, 0, 0),
-            tf2::Quaternion(2, 0, 0, 1),
-        };
-
         // We know that the distance between (0, 0, 0, 1) and (2, 0, 0, 1) is 2
         auto const rotational_distance = 2.;
 
-        auto const const_fn = make_pose_cost_fn(goal, 0, 1.0);
-        CHECK(const_fn({frame}) == Catch::Approx(std::pow(rotational_distance, 2)));
+        auto const const_fn = make_pose_cost_fn(zero_frame, 0, 1.0);
+        CHECK(const_fn({rotate_y2_frame}) == Catch::Approx(std::pow(rotational_distance, 2)));
     }
 
     SECTION("Rotation in one axis, scaled 0.5") {
-        auto const goal = Frame{
-            tf2::Vector3(0, 0, 0),
-            tf2::Quaternion(0, 0, 0, 1),
-        };
-        auto const frame = Frame{
-            tf2::Vector3(0, 0, 0),
-            tf2::Quaternion(2, 0, 0, 1),
-        };
-
         // We know that the distance between (0, 0, 0, 1) and (2, 0, 0, 1) is 2
         auto const rotational_distance = 2.;
         auto const rotation_scale = 0.5;
 
-        auto const const_fn = make_pose_cost_fn(goal, 0, rotation_scale);
+        auto const const_fn = make_pose_cost_fn(zero_frame, 0, rotation_scale);
 
         // The rotation scale is squared before being multiplied by the square of the distance
-        CHECK(const_fn({frame}) ==
+        CHECK(const_fn({rotate_y2_frame}) ==
               Catch::Approx(std::pow(rotational_distance, 2) * std::pow(rotation_scale, 2)));
     }
 
@@ -210,6 +200,49 @@ TEST_CASE("make_pose_cost_fn") {
         auto const cost_fn = make_pose_cost_fn(goal, 0, rotation_scale);
         auto const cost = cost_fn(std::vector<Frame>{frame});
         CHECK(cost == Catch::Approx(error));
+    }
+}
+
+TEST_CASE("make_pose_cost_functions") {
+    auto const goal =
+        Frame{tf2::Vector3(0.3327501714229584, -0.025710120797157288, 0.5902695655822754),
+              tf2::Quaternion(0.9239557003781338,
+                              -0.38249949508300274,
+                              1.324932598914536e-12,
+                              3.2004117980888137e-12)};
+    auto const frame =
+        Frame{tf2::Vector3(0.3327318727877646, -0.02570328270961634, 0.5900141633600922),
+              tf2::Quaternion(0.9239554647443051,
+                              -0.38250006378889556,
+                              1.925047999919496e-05,
+                              2.1223489422435532e-07)};
+    auto const rotation_scale = 0.5;
+
+    SECTION("Function is same as make_pose_cost_fn") {
+        auto const cost_fn = make_pose_cost_fn(goal, 0, rotation_scale);
+        auto const cost_fns = make_pose_cost_functions({goal}, rotation_scale);
+
+        CHECK(cost_fn({frame}) == cost_fns.at(0)({frame}));
+    }
+
+    SECTION("One goal, one function") {
+        auto const cost_fns = make_pose_cost_functions({goal}, rotation_scale);
+        CHECK(cost_fns.size() == 1);
+    }
+
+    SECTION("Two goals, two functions") {
+        auto const cost_fns = make_pose_cost_functions({goal, frame}, rotation_scale);
+        CHECK(cost_fns.size() == 2);
+    }
+
+    SECTION("First goal, tests first frame") {
+        auto const cost_fns = make_pose_cost_functions({goal, frame}, rotation_scale);
+        CHECK(cost_fns.at(0)({goal, frame}) == Catch::Approx(0.));
+    }
+
+    SECTION("Second goal, tests second frame") {
+        auto const cost_fns = make_pose_cost_functions({goal, frame}, rotation_scale);
+        CHECK(cost_fns.at(1)({goal, frame}) == Catch::Approx(0.));
     }
 }
 
