d95244d363df1d8fd34051810603086c957c310e
Chris Thrasher <chrisjthrasher@gmail.com>
2022-10-04 15:56:26 -0600

    Update FindCatch2.cmake

--- cmake/FindCatch2.cmake
+++ cmake/FindCatch2.cmake
@@ -1,10 +1,10 @@
 include(FetchContent)
 
-set(CATCH_INSTALL_DOCS OFF)
-set(CATCH_INSTALL_EXTRAS OFF)
 FetchContent_Declare(Catch2
     GIT_REPOSITORY https://github.com/catchorg/Catch2.git
     GIT_TAG v${Catch2_FIND_VERSION})
 FetchContent_MakeAvailable(Catch2)
 set_target_properties(Catch2 PROPERTIES COMPILE_OPTIONS "")
+get_target_property(CATCH2_INCLUDE_DIRS Catch2 INTERFACE_INCLUDE_DIRECTORIES)
+target_include_directories(Catch2 SYSTEM INTERFACE ${CATCH2_INCLUDE_DIRS})
 include(Catch)
