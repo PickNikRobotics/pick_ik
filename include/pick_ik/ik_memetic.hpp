64c2cdce4c2f7844aa1b8ca91bce29f6068d8413
Sebastian Castro <sebas.a.castro@gmail.com>
2022-10-05 11:13:34 -0600

    Clean up wipeout and mating pool code

--- include/pick_ik/ik_memetic.hpp
+++ include/pick_ik/ik_memetic.hpp
@@ -3,12 +3,13 @@
 #include <pick_ik/goal.hpp>
 #include <pick_ik/robot.hpp>
 
+#include <rsl/random.hpp>
+
 #include <chrono>
 #include <memory>
 #include <moveit/robot_model/joint_model_group.h>
 #include <moveit/robot_model/robot_model.h>
 #include <optional>
-#include <rsl/random.hpp>
 #include <vector>
 
 namespace pick_ik {
@@ -35,14 +36,19 @@ struct MemeticIkParams {
 
 class MemeticIk {
    private:
+    // Evolutionary algorithm values
     std::vector<Individual> population_;
+    std::vector<Individual*> mating_pool_;
     Individual best_;       // Best solution overall.
     Individual best_curr_;  // Best solution so far.
+    std::optional<double> previous_fitness_;
 
+    // Solver parameters
     MemeticIkParams params_;
 
-    // Extinction coefficient gradings (cached since they do not change).
+    // Scaling coefficients (cached since they do not change).
     std::vector<double> extinction_grading_;
+    double inverse_gene_size_;
 
    public:
     MemeticIk(std::vector<double> const& initial_guess, double cost, MemeticIkParams const& params);
@@ -53,6 +59,7 @@ class MemeticIk {
     Individual best() const { return best_; };
     Individual bestCurrent() const { return best_curr_; };
     size_t eliteCount() const { return params_.elite_size; };
+    bool checkWipeout();
     void computeExtinctions();
     void gradientDescent(size_t const i, Robot const& robot, CostFn const& cost_fn);
     void initPopulation(Robot const& robot,
--- src/ik_memetic.cpp
+++ src/ik_memetic.cpp
@@ -25,13 +25,34 @@ MemeticIk::MemeticIk(std::vector<double> const& initial_guess,
     best_ = Individual{initial_guess, cost, 0.0, std::vector<double>(initial_guess.size(), 0.0)};
     best_curr_ = best_;
 
-    // Cache extinction grading coefficients to not have to recompute all the time.
+    population_.reserve(params.population_size);
+    mating_pool_.reserve(params.elite_size);
+
+    // Cache some coefficients to not have to recompute them all the time.
     extinction_grading_.reserve(params.population_size);
     for (size_t i = 0; i < params.population_size; ++i) {
         extinction_grading_.push_back(static_cast<double>(i) /
                                       static_cast<double>(params.population_size - 1));
     }
+    inverse_gene_size_ = 1.0 / static_cast<double>(initial_guess.size());
 };
+
+bool MemeticIk::checkWipeout() {
+    // Handle wipeouts if no progress is being made.
+    if (previous_fitness_.has_value()) {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+        bool const improved =
+            (best_curr_.fitness < *previous_fitness_ - params_.wipeout_fitness_tol);
+#pragma GCC diagnostic pop
+        if (!improved) {
+            return true;
+        }
+    }
+
+    previous_fitness_ = best_curr_.fitness;
+    return false;
+}
 
 void MemeticIk::computeExtinctions() {
     double min_fitness = population_.front().fitness;
@@ -84,31 +105,33 @@ void MemeticIk::initPopulation(Robot const& robot,
     }
 
     computeExtinctions();
+    previous_fitness_.reset();
 }
 
 void MemeticIk::reproduce(Robot const& robot, CostFn const& cost_fn) {
-    std::vector<Individual*> pool;
-    pool.reserve(params_.elite_size);
+    // Reset mating pool
+    mating_pool_.resize(params_.elite_size);
     for (size_t i = 0; i < params_.elite_size; ++i) {
-        pool.emplace_back(&population_[i]);
+        mating_pool_[i] = &population_[i];
     }
 
     for (size_t i = params_.elite_size; i < params_.population_size; ++i) {
-        // Select parents
-        // TODO: Make this code better
-        if (pool.size() > 1) {
-            size_t const idxA = rsl::uniform_int<size_t>(0, pool.size());
+        // Select parents from the mating pool
+        // Note that we permit there being only one parent, which basically counts as just
+        // mutations.
+        if (!mating_pool_.empty()) {
+            size_t const idxA = rsl::uniform_int<size_t>(0, mating_pool_.size());
             size_t idxB = idxA;
-            while (idxB == idxA && pool.size() > 1) {
-                idxB = rsl::uniform_int<size_t>(0, pool.size());
+            while (idxB == idxA && mating_pool_.size() > 1) {
+                idxB = rsl::uniform_int<size_t>(0, mating_pool_.size());
             }
             auto& parentA = population_[idxA];
             auto& parentB = population_[idxB];
 
             // Get mutation probability
             double const extinction = 0.5 * (parentA.extinction + parentB.extinction);
-            double const inverse = 1.0 / static_cast<double>(robot.variables.size());
-            double const mutation_prob = extinction * (1.0 - inverse) + inverse;
+            double const mutation_prob =
+                extinction * (1.0 - inverse_gene_size_) + inverse_gene_size_;
 
             auto const mix_ratio = rsl::uniform_real(0.0, 1.0);
             for (size_t j_idx = 0; j_idx < robot.variables.size(); ++j_idx) {
@@ -131,21 +154,22 @@ void MemeticIk::reproduce(Robot const& robot, CostFn const& cost_fn) {
                 // Clamp to valid joint values
                 gene = std::clamp(gene, joint.clip_min, joint.clip_max);
 
-                // Approximate initial gradient
+                // Approximate gradient
                 population_[i].gradient[j_idx] = gene - original_gene;
             }
 
-            // Evaluate fitness.
+            // Evaluate fitness and remove parents from the mating pool if a child with better
+            // fitness exists.
             population_[i].fitness = cost_fn(population_[i].genes);
-
-            // Remove elements from the mating pool whose children have better fitness.
             if (population_[i].fitness < parentA.fitness)
-                pool.erase(remove(pool.begin(), pool.end(), &parentA), pool.end());
+                mating_pool_.erase(remove(mating_pool_.begin(), mating_pool_.end(), &parentA),
+                                   mating_pool_.end());
             if (population_[i].fitness < parentB.fitness)
-                pool.erase(remove(pool.begin(), pool.end(), &parentB), pool.end());
+                mating_pool_.erase(remove(mating_pool_.begin(), mating_pool_.end(), &parentB),
+                                   mating_pool_.end());
 
         } else {
-            // Roll a new population member randomly.
+            // If the mating pool is empty, roll a new population member randomly.
             for (size_t j_idx = 0; j_idx < robot.variables.size(); ++j_idx) {
                 auto const& var = robot.variables[j_idx];
                 population_[i].genes[j_idx] = rsl::uniform_real(0.0, 1.0) * var.span + var.min;
@@ -161,7 +185,7 @@ void MemeticIk::reproduce(Robot const& robot, CostFn const& cost_fn) {
 void MemeticIk::printPopulation() const {
     fmt::print("Fitnesses:\n");
     for (size_t i = 0; i < populationCount(); ++i) {
-        fmt::print("{}: {:.4f}\n", i, population_[i].fitness);
+        fmt::print("{}: {}\n", i, population_[i].fitness);
     }
     fmt::print("\n");
 }
@@ -170,6 +194,7 @@ void MemeticIk::sortPopulation() {
     std::sort(population_.begin(), population_.end(), [](Individual const& a, Individual const& b) {
         return a.fitness < b.fitness;
     });
+    computeExtinctions();
     best_curr_ = population_[0];
     if (best_curr_.fitness < best_.fitness) {
         best_ = best_curr_;
@@ -195,7 +220,6 @@ auto ik_memetic(std::vector<double> const& initial_guess,
 
     // Main loop
     int iter = 0;
-    std::optional<double> previous_fitness = std::nullopt;
     auto const timeout_point =
         std::chrono::system_clock::now() + std::chrono::duration<double>(timeout);
     while (std::chrono::system_clock::now() < timeout_point) {
@@ -207,37 +231,23 @@ auto ik_memetic(std::vector<double> const& initial_guess,
         // Perform mutation and recombination
         ik.reproduce(robot, cost_fn);
 
-        // Sort fitnesses
+        // Sort fitnesses and update extinctions
         ik.sortPopulation();
-        ik.computeExtinctions();
         if (print_debug) {
             fmt::print("Iteration {}\n", iter);
             ik.printPopulation();
         }
 
-        // Check for termination
+        // Check for termination and wipeout conditions
         if (solution_fn(ik.best().genes)) {
             if (print_debug) fmt::print("Found solution!\n");
             return ik.best().genes;
         }
-
-        // Handle wipeouts if no progress is being made.
-        if (previous_fitness.has_value()) {
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
-            bool const improved =
-                (ik.bestCurrent().fitness < *previous_fitness - params.wipeout_fitness_tol);
-#pragma GCC diagnostic pop
-            if (!improved) {
-                if (print_debug) fmt::print("Population wipeout\n");
-                ik.initPopulation(robot, cost_fn, initial_guess);
-                previous_fitness.reset();
-            } else {
-                previous_fitness = ik.bestCurrent().fitness;
-            }
-        } else {
-            previous_fitness = ik.bestCurrent().fitness;
+        if (ik.checkWipeout()) {
+            if (print_debug) fmt::print("Population wipeout\n");
+            ik.initPopulation(robot, cost_fn, initial_guess);
         }
+
         iter++;
     }
 
